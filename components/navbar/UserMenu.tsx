528121046cf9129bcdb13d2426cea1af49927eef
Tushar Bhatt <tusharbhatt0135@gmail.com>
2023-11-21 23:22:27 +0530

    logo

--- components/navbar/UserMenu.tsx
+++ components/navbar/UserMenu.tsx
@@ -58,7 +58,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
             cursor-pointer
           "
         >
-          Airbnb your home
+          Rent your home
         </div>
         <div 
         onClick={toggleOpen}
